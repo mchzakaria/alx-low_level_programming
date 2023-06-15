a489565bca8463a975bc1deb55190174b725ed2a
mch_zakaria <achrafelghaiti4@gmail.com>
2023-06-15 09:17:28 -0700

    FIN

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(void){
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	retun(0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,9 @@
+include <stdio.h>
+int main(void){
+    printf("Size of a char %d bytes(s)\n",sizeof(char));
+    printf("Size of an int %d bytes(s)\n",sizeof(int));
+    printf("Size of a long int %d bytes(s)\n",sizeof(long int));
+    printf("Size of a long long int %d bytes(s)\n",sizeof(long long int));
+    printf("Size of a float %d bytes(s)\n",sizeof(float));    
+    return(0);
+}
