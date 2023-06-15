ddce1af8af4587167008380b7c25081044a6a06a
mch_zakaria <achrafelghaiti4@gmail.com>
2023-06-15 09:56:59 -0700

    hope

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void){
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	retun(0);
+	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,11 @@
-include <stdio.h>
-int main(void){
-    printf("Size of a char %d bytes(s)\n",sizeof(char));
-    printf("Size of an int %d bytes(s)\n",sizeof(int));
-    printf("Size of a long int %d bytes(s)\n",sizeof(long int));
-    printf("Size of a long long int %d bytes(s)\n",sizeof(long long int));
-    printf("Size of a float %d bytes(s)\n",sizeof(float));    
-    return(0);
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
