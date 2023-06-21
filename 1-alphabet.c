a00b41a60a6433b316c2f7eda126596e83634157
mch_zakaria <achrafelghaiti4@gmail.com>
2023-06-20 17:09:44 -0700

    FIN

---
+++ 1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-0x00. C - Hello, World
+Hi
